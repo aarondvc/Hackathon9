1cf5cd7b604f84316b8da19f3d2fe1ea10e2b46e
Varun Menon <varunmenon2004@gmail.com>
2024-02-10 15:07:43 -0500

    Added the response objects

---
+++ PowerStats.java
@@ -0,0 +1,9 @@
+public class PowerStats {
+    String intelligence;
+    String strength;
+    String speed;
+    String durability;
+    String power;
+    String combat;
+    
+}
---
+++ SuperheroApiResponse.java
@@ -0,0 +1,18 @@
+
+public class SuperheroApiResponse {
+    String heroResponse;
+    String heroID;
+    String heroName;
+    PowerStats heroPowerStats = new PowerStats();
+    Biography heroBiography = new Biography();
+    Appearance heroAppearance = new Appearance();
+    Work heroWork = new Work();
+    Connections heroConnections = new Connections();
+    Image heroImage = new Image();
+
+
+
+
+
+    
+}
