50f550edbf60035c50b0d52f5a8daef1c3305679
aarondvc <adv69240@uga.edu>
2024-02-10 13:09:35 -0500

    GUI Elements

--- HeroQuiz.java
+++ HeroQuiz.java
@@ -4,11 +4,37 @@ import javax.swing.*;
 
 public class HeroQuiz implements ActionListener{
    String [] questions = {
-      "What is the powerstat for the Hulk?",
-   }
+      "What is the for the Hulk?", 
+   };
    
-   public Quiz() {
+   char userAns;
+   char correctAns;
+   int index;
+   int correctUserAns = 0;
+   int totalQuestions = questions.length;
+   int result;
+   
+   JFrame frame = new JFrame();
+   JTextField textField = new JTextField();
+   JTextArea textArea = new JTextArea();
+   JButton aBtn = new JButton();
+   JButton bBtn = new JButton();
+   JButton cBtn = new JButton();
+   JButton dBtn = new JButton();
+   JLabel aLbl = new JLabel();
+   JLabel bLbl = new JLabel();
+   JLabel cLbl = new JLabel();
+   JLabel dLbl = new JLabel();
+   ImageIcon display = new ImageIcon();
 
+
+   public HeroQuiz() {
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(650,650);
+
+      
+
+      frame.setVisible(true);
    }
 
    public void nextQuestion() {
--- QuizDriver.java
+++ QuizDriver.java
@@ -1,6 +1,6 @@
 public class QuizDriver {
     
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        HeroQuiz quiz = new HeroQuiz();
     }
 }
