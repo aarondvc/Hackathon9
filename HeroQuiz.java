f83a4637b1bcf90b3a8069ee46385df77fc3a421
aarondvc <adv69240@uga.edu>
2024-02-10 14:56:34 -0500

    gui commit 3

--- HeroQuiz.java
+++ HeroQuiz.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class HeroQuiz implements ActionListener{
    String [] questions = {
-      "What is the for the Hulk?", 
+      "What is the Hulk's strength level?", "What comic was Batman's first appearance?",  
    };
    
    char userAns;
@@ -16,6 +16,7 @@ public class HeroQuiz implements ActionListener{
    
    JFrame frame = new JFrame();
    JTextField textField = new JTextField();
+   JLabel infoLbl = new JLabel();
    JTextArea textArea = new JTextArea();
    JButton aBtn = new JButton();
    JButton bBtn = new JButton();
@@ -25,6 +26,7 @@ public class HeroQuiz implements ActionListener{
    JLabel bLbl = new JLabel();
    JLabel cLbl = new JLabel();
    JLabel dLbl = new JLabel();
+   JButton playBtn = new JButton();
    ImageIcon display = new ImageIcon("resources/byte.jpeg");
    JLabel displayLbl = new JLabel();
 
@@ -35,18 +37,26 @@ public class HeroQuiz implements ActionListener{
       frame.getContentPane().setBackground(new Color(136,206,235));
       frame.setResizable(false);
       frame.setLayout(null);
-      textField.setBounds(0, 0, 400, 50);
+
+      textField.setBounds(0, 25, 400, 50);
       textField.setBackground(null);
       textField.setForeground(new Color(186,12,47));
-      textField.setFont(new Font("Arial",Font.BOLD, 30));
+      textField.setFont(new Font("Arial",Font.BOLD, 36));
       textField.setBorder(BorderFactory.createBevelBorder(5));
       textField.setHorizontalAlignment(JTextField.CENTER);
       textField.setEditable(false);
       textField.setText("SuperHero Quiz");
-      displayLbl.setBounds(0, 50, 200, 200);
+
+      displayLbl.setBounds(0, 175, 400, 200);
       displayLbl.setIcon(display);
+      displayLbl.setHorizontalAlignment(JLabel.CENTER);
+      infoLbl.setBounds(0, 75, 400, 50);
+      infoLbl.setText("How well do you know your favorite superheroes?");
+      infoLbl.setFont(new Font("Arial",Font.BOLD, 28));
+   
       frame.add(displayLbl);
       frame.add(textField);
+      frame.add(infoLbl);
      
       frame.setVisible(true);
    }
