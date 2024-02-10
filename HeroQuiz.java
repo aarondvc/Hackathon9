65032c52db339f9c5808c33e3e4b72a202e67848
varunm2004 <varunmenon2004@gmail.com>
2024-02-10 18:36:11 -0500

    Major commit with hard-code

--- HeroQuiz.java
+++ HeroQuiz.java
@@ -4,9 +4,35 @@ import javax.swing.*;
 
 public class HeroQuiz implements ActionListener{
    String [] questions = {
-      "What is the Hulk's strength level?", "What comic was Batman's first appearance?",  
+       
+      "Which superhero is known as the 'Man of Steel'?" ,
+      "What is the name of Spider-Man's alter-ego?",
+      "Who is the king of Wakanda and also known as the Black Panther",
+      "What is the real name of the superhero known as Wonder Woman",
+      "What is the civilian name of the superhero known as Captain America?",
+      "Which superhero is known for wielding a magical hammer called Mjolnir?"
+   };
+
+   String[][] choices = {
+                           {"Thor", "Superman", "Iron Man", "Wolverine"},
+                           {"Barry Allen", "Steve Rodgers", "Peter Parker", "Bruce Banner"},
+                           {"T'Challa", "Bruce Wayne", "Tony Stark", "Clark Kent"},
+                           {"Diana Prince", "Natasha Romanoff", "Jessica Jones", "Carol Danvers"},
+                           {"Tony Stark", "Bruce Wayne", "Clark Kent", "Steve Rodgers"},
+                           {"Hulk", "Thor", "Captain America", "Green Lantern"}
+   };
+
+   char[] answers = {
+      'A',
+      'C',
+      'A',
+      'A',
+      'D',
+      'B'
    };
    
+   //What is the Hulk's strength level
+   //What comic was Batman's first appearance?
    char userAns;
    char correctAns;
    int index;
@@ -14,10 +40,13 @@ public class HeroQuiz implements ActionListener{
    int totalQuestions = questions.length;
    int result;
    
+   
    JFrame frame = new JFrame();
-   JTextField title = new JTextField();
-   JLabel infoLbl = new JLabel();
-   JTextArea questionArea = new JTextArea();
+   JTextField textfield = new JTextField();
+   JTextArea textArea = new JTextArea();
+   
+   JLabel infoLbl = new JLabel(); 
+
    JButton aBtn = new JButton();
    JButton bBtn = new JButton();
    JButton cBtn = new JButton();
@@ -27,57 +56,155 @@ public class HeroQuiz implements ActionListener{
    JLabel cLbl = new JLabel();
    JLabel dLbl = new JLabel();
    JButton playBtn = new JButton();
-   ImageIcon display = new ImageIcon("resources/byte.jpeg");
-   JLabel displayLbl = new JLabel();
+   //ImageIcon display = new ImageIcon("resources/byte.jpeg");
+   //JLabel displayLbl = new JLabel();
+
+   JTextField number_right = new JTextField();
+   JTextField percentage = new JTextField();
 
 
    public HeroQuiz() {
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setSize(400,650);
-      frame.getContentPane().setBackground(new Color(136,206,235));
+      frame.setSize(650,650);
+      frame.getContentPane().setBackground(new Color(108,183,240));
       frame.setResizable(false);
       frame.setLayout(null);
 
-      title.setBounds(0, 25, 400, 50);
-      title.setBackground(null);
-      title.setForeground(new Color(186,12,47));
-      title.setFont(new Font("Arial",Font.BOLD, 36));
-      title.setBorder(BorderFactory.createBevelBorder(5));
-      title.setHorizontalAlignment(JTextField.CENTER);
-      title.setEditable(false);
-      title.setText("SuperHero Quiz");
+      //textfield.setBounds(0, 25, 400, 50);
+      textfield.setBounds(0,0,650,50);
+      textfield.setBackground(new Color(108,183,240));
+      textfield.setForeground(new Color(155,0,0));
+      textfield.setFont(new Font("Arial",Font.BOLD, 30));
+      textfield.setBorder(BorderFactory.createBevelBorder(5));
+      textfield.setHorizontalAlignment(JTextField.CENTER);
+      textfield.setEditable(false);
+      textfield.setText("Superhero Quiz");
 
-      questionArea.setBounds(0, 175, 400, 50);
-      questionArea.setLineWrap(true);
-      questionArea.setWrapStyleWord(true);
-      questionArea.setBackground(null);
-      questionArea.setForeground(new Color(186,12,47));
-      questionArea.setFont(new Font("Arial",Font.BOLD, 20));
-      questionArea.setBorder(BorderFactory.createBevelBorder(5));
-      questionArea.setEditable(false);
-      questionArea.setText("trial");
+      textArea.setBounds(0, 50, 650, 50);
+      textArea.setLineWrap(true);
+      textArea.setWrapStyleWord(true);
+      textArea.setBackground(null);
+      textArea.setForeground(new Color(155,0,0));
+      textArea.setFont(new Font("Arial",Font.BOLD, 25));
+      textArea.setBorder(BorderFactory.createBevelBorder(5));
+      textArea.setEditable(false);
+      //textArea.setText("SAMPLE TEXT");
 
-      displayLbl.setBounds(0, 125, 400, 200);
-      displayLbl.setIcon(display);
-      displayLbl.setHorizontalAlignment(JLabel.CENTER);
+      aBtn.setBounds(0,100,100,100);
+      aBtn.setFont(new Font("Ariel",Font.BOLD, 35));
+      aBtn.setFocusable(false);
+      aBtn.addActionListener(this);
+      aBtn.setText("A");
 
-      frame.add(displayLbl);
-      frame.add(questionArea);
-      frame.add(title);
+      bBtn.setBounds(0,200,100,100);
+      bBtn.setFont(new Font("Ariel",Font.BOLD, 35));
+      bBtn.setFocusable(false);
+      bBtn.addActionListener(this);
+      bBtn.setText("B");
+
+      cBtn.setBounds(0,300,100,100);
+      cBtn.setFont(new Font("Ariel",Font.BOLD, 35));
+      cBtn.setFocusable(false);
+      cBtn.addActionListener(this);
+      cBtn.setText("C");
+
+      dBtn.setBounds(0,400,100,100);
+      dBtn.setFont(new Font("Ariel",Font.BOLD, 35));
+      dBtn.setFocusable(false);
+      dBtn.addActionListener(this);
+      dBtn.setText("D");
+
+      aLbl.setBounds(125,100,500,100);
+      aLbl.setBackground(new Color(50,50,50));
+      aLbl.setForeground(new Color(155,0,0));
+      aLbl.setFont(new Font("Arial",Font.BOLD,35));
+      //aLbl.setText("Testing label 1");
+
+      bLbl.setBounds(125,200,500,100);
+      bLbl.setBackground(new Color(50,50,50));
+      bLbl.setForeground(new Color(155,0,0));
+      bLbl.setFont(new Font("Arial",Font.BOLD,35));
+      //bLbl.setText("Testing label 2");
+
+      cLbl.setBounds(125,300,500,100);
+      cLbl.setBackground(new Color(50,50,50));
+      cLbl.setForeground(new Color(155,0,0));
+      cLbl.setFont(new Font("Arial",Font.BOLD,35));
+      //cLbl.setText("Testing label 3");
+      
+      dLbl.setBounds(125,400,500,100);
+      dLbl.setBackground(new Color(50,50,50));
+      dLbl.setForeground(new Color(155,0,0));
+      dLbl.setFont(new Font("Arial",Font.BOLD,35));
+      //dLbl.setText("Testing label 4");
+      
+      frame.add(aLbl);
+      frame.add(bLbl);
+      frame.add(cLbl);
+      frame.add(dLbl);
+
+      //picture settings
+      //displayLbl.setBounds(125, 50, 400, 200);
+      //displayLbl.setIcon(display);
+      //displayLbl.setHorizontalAlignment(JLabel.CENTER);
+
+      //add picture to the frame
+      //frame.add(displayLbl);
+      
+      frame.add(textArea);
+      frame.add(textfield);
       frame.add(aBtn);
       frame.add(bBtn);
       frame.add(cBtn);
       frame.add(dBtn);
-     
+      number_right.setBounds(225,225,200,100);
+      number_right.setBackground(new Color(25,25,25));
+      number_right.setForeground(new Color(155,0,0));
+      number_right.setFont(new Font ("Arial", Font.BOLD, 50));
+      number_right.setBorder(BorderFactory.createBevelBorder(1));
+      number_right.setHorizontalAlignment(JTextField.CENTER);
+      number_right.setEditable(false);
+
+      percentage.setBounds(225,325,200,100);
+      percentage.setBackground(new Color(25,25,25));
+      percentage.setForeground(new Color(155,0,0));
+      percentage.setBorder(BorderFactory.createBevelBorder(1));
+      percentage.setHorizontalAlignment(JTextField.CENTER);
+      percentage.setEditable(false);
+
+      
       frame.setVisible(true);
+
+      nextQuestion();
    }
 
    public void nextQuestion() {
 
+      if(index>=totalQuestions) {
+         results();
+      } else {
+         textfield.setText("Question " + (index + 1));
+         textArea.setText(questions[index]);
+         aLbl.setText(choices[index][0]);
+         bLbl.setText(choices[index][1]);
+         cLbl.setText(choices[index][2]);
+         dLbl.setText(choices[index][3]);
+      }
    }
 
    @Override
    public void actionPerformed(ActionEvent e) {
+      aBtn.setEnabled(false);
+      bBtn.setEnabled(false);
+      cBtn.setEnabled(false);
+      dBtn.setEnabled(false);
+
+      if(e.getSource()==aBtn) {
+         if(correctAns == answers[index]) {
+
+         }
+      }
+
       
    }
 
